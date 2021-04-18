c80befa460dcadd2f605181153aa28df970052ba
Prakhar Jain <prakhartkg@gmail.com>
2021-04-19 04:09:06 +0530

    Initaial Commit

---
+++ src/test/java/com/pinecone/cloud/config/server/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinecone.cloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
